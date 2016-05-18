b8ade3bdf4d433ace96f2bc81afa51b90bfbb619
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-05-18 21:51:39 +0100

    Created webapp

---
+++ webapp/src/main/java/jachemkit/webapp/Application.java
@@ -0,0 +1,28 @@
+package jachemkit.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.neo4j.NodeEntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableNeo4jRepositories("jachemkit.hashchem.neo")
+@NodeEntityScan("jachemkit.hashchem.neo")
+public class Application extends SpringBootServletInitializer {
+
+	@Bean
+	public Module getGuavaModule() {
+	  return new GuavaModule();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
